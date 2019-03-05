47bfabec624bf0b050e81ecb6e7723f985421f7c
Phuong Tran <tranduyphuong2110@gmail.com>
2019-03-04 20:27:13 -0800

    todo list

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => res.render("index"));
 
+
+app.get('/getBoundingBox', (req,  res)=> res.send())
 app.listen(3000,  () => console.log("Example app listening on port 3000!"));
