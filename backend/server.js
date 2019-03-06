e31a98b64f4186889546399d1c16120638d3f6c0
Phuong Tran <tranduyphuong2110@gmail.com>
2019-03-05 23:25:42 -0800

    route tests

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,11 @@ app.use(express.static('public'))
  
 app.get('/', (req, res) => res.render("index"));
 
+app.get('/home', (req, res) => {
+    res.send("this is home page");
+});
 
-app.get('/getBoundingBox', (req,  res)=> res.send())
+app.get('/getbbox', (req,  res) => {
+    res.send("hello");
+});
 app.listen(3000,  () => console.log("Example app listening on port 3000!"));
