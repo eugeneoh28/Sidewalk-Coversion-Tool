cddb480b97a0a48cce1c4f66df7657deee389d3c
Phuong Tran <tranduyphuong2110@gmail.com>
2019-03-10 04:43:17 -0700

    using postman to test post from client and return a linestring

--- backend/server.js
+++ backend/server.js
@@ -5,153 +5,156 @@ let lineInBox = require("./controllers/InnerLineString");
 
 
 app.use(express.static('public'))
- 
-app.get('/', (req, res) => res.render("index"));
+app.use(express.json());
 
-app.get('/home', (req, res) => {
-    res.send("this is home page");
-});
+// app.get('/', (req, res) => res.render("index"));
 
-app.get('/getbbox', (req,  res) => {
+// app.get('/home', (req, res) => {
+//     res.send("this is home page");
+// });
+
+var lineString = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.30985224246979,
+            47.67761496987505
+          ],
+          [
+            -122.3077118396759,
+            47.67764025296123
+          ],
+          [
+            -122.30767965316772,
+            47.675845123395696
+          ],
+          [
+            -122.30981469154358,
+            47.67582706342832
+          ],
+          [
+            -122.30985760688782,
+            47.677636641092555
+          ]
+        ]
+      }
+    }
+  ]
+}
+
+var outside = {
+  "type": "Feature",
+  "properties": {},
+  "geometry": {
+    "type": "Polygon",
+    "coordinates": [
+      [
+        [
+          -122.30938017368317,
+          47.67784612906428
+        ],
+        [
+          -122.30838239192961,
+          47.67784612906428
+        ],
+        [
+          -122.30838239192961,
+          47.678420410742426
+        ],
+        [
+          -122.30938017368317,
+          47.678420410742426
+        ],
+        [
+          -122.30938017368317,
+          47.67784612906428
+        ]
+      ]
+    ]
+  }
+}
+
+var half = {
+  "type": "Feature",
+  "properties": {},
+  "geometry": {
+    "type": "Polygon",
+    "coordinates": [
+      [
+        [
+          -122.30825364589691,
+          47.676740889933065
+        ],
+        [
+          -122.30724513530731,
+          47.676740889933065
+        ],
+        [
+          -122.30724513530731,
+          47.67722127452346
+        ],
+        [
+          -122.30825364589691,
+          47.67722127452346
+        ],
+        [
+          -122.30825364589691,
+          47.676740889933065
+        ]
+      ]
+    ]
+  }
+}
+var inside = {
+  "type": "Feature",
+  "properties": {},
+  "geometry": {
+    "type": "Polygon",
+    "coordinates": [
+      [
+        [
+          -122.3103404045105,
+          47.675664523440744
+        ],
+        [
+          -122.30692863464354,
+          47.675664523440744
+        ],
+        [
+          -122.30692863464354,
+          47.67779556307916
+        ],
+        [
+          -122.3103404045105,
+          47.67779556307916
+        ],
+        [
+          -122.3103404045105,
+          47.675664523440744
+        ]
+      ]
+    ]
+  }
+}
+var line = turf.lineString([[-74, 40], [-78, 42], [-82, 35]]);
+app.post('/getbbox', (req, res) => {
     //TODO: get a bounding box
     // send back the lines that are within that bbox
 
-    var data = {
-      "type": "FeatureCollection",
-      "features": [
-        {
-          "type": "Feature",
-          "properties": {},
-          "geometry": {
-            "type": "LineString",
-            "coordinates": [
-              [
-                -122.30985224246979,
-                47.67761496987505
-              ],
-              [
-                -122.3077118396759,
-                47.67764025296123
-              ],
-              [
-                -122.30767965316772,
-                47.675845123395696
-              ],
-              [
-                -122.30981469154358,
-                47.67582706342832
-              ],
-              [
-                -122.30985760688782,
-                47.677636641092555
-              ]
-            ]
-          }
-        }
-      ]
-    }
-
-    var outside = {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [
-              -122.30938017368317,
-              47.67784612906428
-            ],
-            [
-              -122.30838239192961,
-              47.67784612906428
-            ],
-            [
-              -122.30838239192961,
-              47.678420410742426
-            ],
-            [
-              -122.30938017368317,
-              47.678420410742426
-            ],
-            [
-              -122.30938017368317,
-              47.67784612906428
-            ]
-          ]
-        ]
-      }
-    }
-
-    var half = {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [
-              -122.30825364589691,
-              47.676740889933065
-            ],
-            [
-              -122.30724513530731,
-              47.676740889933065
-            ],
-            [
-              -122.30724513530731,
-              47.67722127452346
-            ],
-            [
-              -122.30825364589691,
-              47.67722127452346
-            ],
-            [
-              -122.30825364589691,
-              47.676740889933065
-            ]
-          ]
-        ]
-      }
-    }
-    var inside = {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [
-              -122.3103404045105,
-              47.675664523440744
-            ],
-            [
-              -122.30692863464354,
-              47.675664523440744
-            ],
-            [
-              -122.30692863464354,
-              47.67779556307916
-            ],
-            [
-              -122.3103404045105,
-              47.67779556307916
-            ],
-            [
-              -122.3103404045105,
-              47.675664523440744
-            ]
-          ]
-        ]
-      }
+    var lineWithin = lineInBox.getLineString(turf.bbox(req.body), lineString.features[0]);
+    if (lineWithin === undefined){
+      res.send("undefined");
+    }else{
+      res.send(lineWithin);
+      
     }
     
-   
-    console.log("line");
-    console.log(lineInBox.getLineString(inside, data.features[0]).geometry.coordinates);
-
-    console.log("----------------------------------")
-    res.send("hello");
-
+    console.log("--------------end------------------")
 });
 app.listen(3000,  () => console.log("Example app listening on port 3000!"));
