3ed3746f49b4b33adaa7bbaa1dbe04c5079ad6dd
Eugene Oh <eugene.oh.28@gmail.com>
2019-03-13 13:34:22 -0700

    first prototype

---
+++ src/components/validateQuestions.js
@@ -0,0 +1,28 @@
+var data = {
+        "sidewalk": [
+            {
+                "question": " what is 1 + 1?",
+                "answers": ["1","2","3","4"]    
+            },
+            {
+                "question": " what is 1 + 2?",
+                "answers": ["1","2","3","4"] 
+            },
+            {
+                "question": " what is 1 * 2?",
+                "answers": ["1","2","3","4"] 
+            }   
+        ],
+        "crossing": [
+            {
+                "question": "Are there raised curbs on this crossing?",
+                "answers": ["Yes", "No"]
+            },
+            {
+                "question": "Are there markings on this crossing?",
+                "answers": ["Yes", "No"]
+            }
+        ]
+
+    }
+export default data
