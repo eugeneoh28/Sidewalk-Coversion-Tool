1bba36bd45587220ff7830229cd3454ccab76bb6
Eugene Oh <eugene.oh.28@gmail.com>
2019-04-16 21:17:49 -0700

    somewhat done not really

--- src/components/container/HomeContainer.js
+++ src/components/container/HomeContainer.js
@@ -11,12 +11,19 @@ class MainContainer extends Component {
         super();
         this.state = {
                 // [ latitude, longitude]
-                coord : [47.604034, -122.33451],
+                coord : [],
+                lat: "",
+                long: "",
                 layers: new L.FeatureGroup(),
+                validatedData : {},
                 validation: false
         };
 
         this.onNextClicked = this.onNextClicked.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.changeLat = this.changeLat.bind(this);
+        this.changeLong = this.changeLong.bind(this);
+
     }
 
     //update this.state
@@ -32,16 +39,41 @@ class MainContainer extends Component {
 
     //set isNextClicked to true when "Next" button clicked
     onNextClicked(){
-        console.log(this.state.layers.toGeoJSON());
         this.setState({
+            validatedData : this.state.layers.toGeoJSON(),
             validation : true
         })
     }
 
+    updateData(data) {
+        this.setState({
+            validatedData: data
+        })
+        console.log(this.state.validatedData)
+    }
+
+    handleSubmit() {
+        let lat = this.state.lat
+        let long = this.state.long
+        this.setState({
+            coord : [lat, long]
+        })
+    }
+
+    changeLat(e) {
+        this.setState({
+            lat: e.target.value
+        })
+    }
+
+    changeLong(e) {
+        this.setState({
+            long: e.target.value
+        })
+    }
+
     render(){
-        return (
-            <div className="main">
-                <NavBar onNextClicked={() => this.onNextClicked()}></NavBar>
+        const map = (
                 <div className="container">
                     <div className="row">
                         <div className="col">
@@ -50,14 +82,34 @@ class MainContainer extends Component {
                         <div className="col">
                             <StreetViewContainer coord={this.state.coord} reFocusCallback={(lat, lng) => this.reFocus(lat,lng)} />
                         </div>
-                        {/* * After "Next" button clicked, the button would be replace by ValidationContainer */}
                     </div>
                     <div className="row">
                         <div className="col">
-                            {this.state.validation ? <ValidationContainer/> : null}
+                            {this.state.validation ? <ValidationContainer data={this.state.validatedData} validateCallback={(data) => this.updateData(data)}/> : null}
                         </div>
                     </div>
                 </div>
+            )
+
+        const setCoord = (
+                <div className="container">
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                          <p>Lat:</p>
+                          <textarea onChange={this.changeLat}/>
+                          <p>Long:</p>
+                          <textarea onChange={this.changeLong} />
+                        </label>
+                        <br />
+                        <input type="submit" value="Submit" />
+                      </form>
+                </div>
+            )
+
+        return (
+            <div className="main">
+                <NavBar onNextClicked={() => this.onNextClicked()}></NavBar>
+                {this.state.coord.length ? map : setCoord}
             </div>
         );
     }
